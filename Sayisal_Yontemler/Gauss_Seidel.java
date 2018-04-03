949de31d670b6e6511cf28169f9f4147ab768bb8
ibrahim <ibrahim-akin@msn.com>
2018-04-03 14:26:20 +0300

    Update Gauss_Seidel.java

--- Sayisal_Yontemler/Gauss_Seidel.java
+++ Sayisal_Yontemler/Gauss_Seidel.java
@@ -6,7 +6,7 @@
 // x2 = (-19.3 - 0.1*x1 + 0.3*x3)/7
 // x3 = (71.4 - 0.3*x1 + 0.2*x2)/10
 
-public class Gauss_Siedel {
+public class Gauss_Seidel {
 	public static void main(String[] args) {
 		double x1=0, x2=0, x3=0;
 		for (int i = 0; i < 20; i++) {
